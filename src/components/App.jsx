dc2649ae4004dfa27c3c297facf28e1bc0000434
Lesya <lesyazhornyk50@gmail.com>
2023-11-09 20:07:39 +0100

    Select

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import { Name } from './ContactForm/ContactForm.styled';
+import { NameItem } from './ContactForm/ContactForm.styled';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        marginLeft: '2.5rem',
       }}
     >
-      React homework template
+      <Name>Phonebook</Name>
+      <ContactForm />
+
+      <NameItem>Contacts</NameItem>
+      <Filter />
+      <ContactList />
     </div>
   );
 };
